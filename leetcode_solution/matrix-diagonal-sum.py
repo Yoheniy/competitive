aa21f6c45eb4a3ada658054e1c6a6efa371fdc2c
yohannes welel <125876217+Yoheniy@users.noreply.github.com>
2024-01-04 17:02:59 +0300

    Added solution for Matrix Diagonal Sum

--- leetcode_solution/matrix-diagonal-sum.py
+++ leetcode_solution/matrix-diagonal-sum.py
@@ -1,19 +1,14 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        sums=0
         n=len(mat)
-        for i in range(len(mat)):
-            sums+=mat[i][i]
-        k=0
-        for j in range(n-1,-1,-1):
-            sums+=mat[j][k]
-            k+=1
-        if n%2==0:
-            return sums
-        else:
-            i=n//2
-            sums-=mat[i][i]
-            return sums
+        ans=0
+        for i in range(n):
+            ans+=mat[i][i]
+            ans+=mat[n-1-i][i]
+        if n%2!=0:
+            ans-=mat[n//2][n//2]
+        return ans
+
             
 
         
