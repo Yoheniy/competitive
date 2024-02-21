c94daec5bbe3b0db5b03d7d96c63515d3c9bf8ae
yohannes welel <125876217+Yoheniy@users.noreply.github.com>
2024-02-21 18:52:17 +0300

    Add solution for Rabbits in Forest

--- leetcode_solution/leetcode/rabbits-in-forest.py
+++ leetcode_solution/leetcode/rabbits-in-forest.py
@@ -1,14 +1,17 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        c={}
+        c=Counter(answers)
         ans=0
-        for el in answers:
-            if el==0:
-                ans+=1
-            else:
-                if el not in c or el==c[el]:
-                    c[el]=0
-                    ans+=el+1
-                else:
-                    c[el]+=1
-        return ans
+        for key,value in c.items():
+            temp=ceil(value/(key+1))
+            ans+=(temp)*(key+1)
+        return ans 
+        #example we have [1,1,2]
+        #our counter c={1:2,2:1}
+        #1 element means other than me  there is 1 elements this means we are 2
+        #2 element means other than me  there is 1 elements this means we are 3
+        #the frequency element occur division by the number of that group it give us in how many group we divide them
+        #and multiplication of the amount of group and the total number of element in the group gives us the answer
+        
+
+        
